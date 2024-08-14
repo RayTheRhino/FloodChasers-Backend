8c22401dc80b771f92c222451b3d24da308322e3
Chen Maimon <chenmay1696@gmail.com>
2024-08-14 20:06:15 +0300

    Updated the date format

--- FloodChasersModel/Boundaries/Alerts/AlertBoundary.cs
+++ FloodChasersModel/Boundaries/Alerts/AlertBoundary.cs
@@ -12,8 +12,8 @@ namespace FloodChasersModel.Boundaries.Alerts
         public string? Id {  get; set; }
         public string Headline {  get; set; }
         public string Description { get; set; }
-        public DateTime TimeCreated { get; set; }
-        public DateTime TimeUpdated { get; set; }
+        public DateTimeOffset TimeCreated { get; set; }
+        public DateTimeOffset TimeUpdated { get; set; }
         public string Severity { get; set; }
         public string Areas { get; set; }
         public double Lat { get; set; }
